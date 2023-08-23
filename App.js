9e0923135a49593fff12695ba4b74902e545f8ad
ansung96 <133179994+ansung96@users.noreply.github.com>
2023-08-23 16:49:12 +0900

    ㅁㄴㅇ
    ㅁㄴㅇ

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import "./styles.css";
+
+// don't change the Component name "App"
+export default function App() {
+  return (
+    <div>
+      <h1>Todos</h1>
+
+      <div className="card">
+        <p>
+          Please note: Below are just the most important todos - feel free to
+          add more.
+        </p>
+      </div>
+
+      <ul>
+        <li className="todo">
+          <div className="card">
+            <h2>Learn React</h2>
+            <p>Learn React fundamentals & explore core concepts</p>
+          </div>
+        </li>
+        <li className="todo">
+          <div className="card">
+            <h2>Practice React</h2>
+            <p>Apply your knowledge & build demo projects</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+}
